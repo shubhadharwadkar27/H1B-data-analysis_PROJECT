155bf2311296f4d8fc8a5ea9edd18f9d6b9669c7
shubhadharwadkar27 <shubhadharwadkar27@gmail.com>
2017-04-19 14:13:12 +0530

    Top 5 Employers who file most petitions every year

--- top_5 Employers.sql
+++ top_5 Employers.sql
@@ -13,7 +13,6 @@ LOAD DATA LOCAL INPATH '/home/shubha/PROJECT/sample.csv' OVERWRITE INTO TABLE h1
 
  describe h1b_1;
 
-
 select employer_name, count(case_status) as temp, year from h1b_1 where case_status ='CERTIFIED' and year= '2011' group by year,employer_name order by temp desc limit 5;
 select employer_name, count(case_status) as temp, year from h1b_1 where case_status ='CERTIFIED' and year= '2012' group by year,employer_name order by temp desc limit 5;
 select employer_name, count(case_status) as temp, year from h1b_1 where case_status ='CERTIFIED' and year= '2013' group by year,employer_name order by temp desc limit 5;
